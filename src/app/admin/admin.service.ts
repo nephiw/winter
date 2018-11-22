f731c86090edffb82604e604b4f418f6d295def8
Nephi Wright <nephiw@gmail.com>
2018-11-21 22:06:10 -0700

    Add votes to the contact table.

--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -16,21 +16,32 @@ export class AdminService {
   public getAllContacts(): Observable < any[] > {
     const entriesRef = this.db.collection('/entries').valueChanges();
     const contactsRef = this.db.collection('/contacts').valueChanges();
+    const votesRef = this.db.collection('/votes').valueChanges();
 
-    return zip(entriesRef, contactsRef).pipe(
-      map(([houses, contacts]) => {
+    return zip(entriesRef, contactsRef, votesRef).pipe(
+      map(([houses, contacts, votes]) => {
         const results = [];
 
         contacts.forEach((contact: Contact) => {
           const house = houses.find((h: HouseEntry) => h.houseAddress === contact.houseAddress) as HouseEntry;
 
           results.push(Object.assign({
-            houseAddress: house ? house.houseAddress : ''
+            houseAddress: house ? house.houseAddress : '',
+            votes: votes ? this.getVoteCount(votes, house.houseAddress) : 0
           }, contact));
         });
         return results;
       })
     );
+  }
+
+  private getVoteCount(votes: any[], houseAddress: string): number {
+    return votes.reduce((acc, vote) => {
+      if ((vote as any).houseAddress === houseAddress) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
   }
 
   // public getVotes(): Observable <any[]> {
